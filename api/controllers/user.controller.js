e169628db179d6725e3d00fb112fe432fad7db74
SHergibo <sachau888@hotmail.com>
2021-07-01 13:54:05 +0200

    fix: Deleted use of token auth when creating a new user in add user controller.

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,53 +1,51 @@
 const User = require('./../models/user.model'),
-      Boom = require('@hapi/boom'),
-      TokenAuth = require('./../models/token-auth.model');
+      Boom = require('@hapi/boom');
 
 /**
 * Post one user
 */
-exports.add = async (req, res, next) =>{
-    try{
-        const user = new User(req.body);
-        await user.save();
-        await TokenAuth.generate(user);
-        return res.json(user.transform());
-    }catch(error){
-        next({error: error, boom: User.checkDuplicateEmail(error)});
-    }
+exports.add = async (req, res, next) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    return res.json(user.transform());
+  } catch (error) {
+    next({ error: error, boom: User.checkDuplicateEmail(error) });
+  }
 };
 
 /**
 * GET one user
 */
-exports.findOne = async (req, res, next) =>{
-    try {
-        const user = await User.findById(req.params.userId);
-        return res.json(user.transform());
-    } catch (error) {
-        next({error: error, boom: Boom.badImplementation(error.message)});      
-    }
+exports.findOne = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    return res.json(user.transform());
+  } catch (error) {
+    next({ error: error, boom: Boom.badImplementation(error.message) });
+  }
 };
 
 /**
 * PATCH user
 */
 exports.update = async (req, res, next) => {
-    try {
-        const user = await User.findByIdAndUpdate(req.params.userId,  req.body);
-        return res.json(user.transform());
-    } catch (error) {
-        next({error: error, boom: User.checkDuplicateEmail(error)});
-    }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.userId, req.body);
+    return res.json(user.transform());
+  } catch (error) {
+    next({ error: error, boom: User.checkDuplicateEmail(error) });
+  }
 };
 
 /**
 * DELETE user
 */
 exports.remove = async (req, res, next) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.userId);
-        return res.json(user.transform());
-    } catch (error) {
-        next({error: error, boom: Boom.badImplementation(error.message)});         
-    }
+  try {
+    const user = await User.findByIdAndDelete(req.params.userId);
+    return res.json(user.transform());
+  } catch (error) {
+    next({ error: error, boom: Boom.badImplementation(error.message) });
+  }
 };
