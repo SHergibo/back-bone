838c0d1bc8eb6d12f74295eddbd7304b78f8ca1f
SHergibo <sachau888@hotmail.com>
2022-01-26 16:41:18 +0100

    fix: Deleted /register route in auth.route.js.

--- api/routes/v1/auth.route.js
+++ api/routes/v1/auth.route.js
@@ -1,22 +1,12 @@
-const Express = require('express'),
-      AuthController = require(`${process.cwd()}/api/controllers/auth.controller`);
+const Express = require("express"),
+  AuthController = require(`${process.cwd()}/api/controllers/auth.controller`);
 
 const router = Express.Router();
 
-router
-    .route('/register')
-        .post(AuthController.register);
+router.route("/login").post(AuthController.login);
 
-router
-    .route('/login')
-        .post(AuthController.login);
+router.route("/refresh-token").post(AuthController.refresh);
 
-router
-    .route('/refresh-token')
-        .post(AuthController.refresh);
-
-router
-    .route('/logout')
-        .post(AuthController.logout);
+router.route("/logout").post(AuthController.logout);
 
 module.exports = router;
