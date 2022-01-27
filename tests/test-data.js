1cdc826b35613b322dbcbda733083c9f68c58caf
SHergibo <sachau888@hotmail.com>
2022-01-27 14:57:52 +0100

    feat: Added adminOneDataComplete object in test-data.js.

---
+++ tests/test-data.js
@@ -0,0 +1,8 @@
+module.exports.adminOneDataComplete = {
+  username: "John Doe",
+  firstname: "John",
+  lastname: "Doe",
+  email: "johndoe@test.com",
+  password: "123456789",
+  role: "admin",
+};
