c901732958dbe3afa730153b696c895a930c941e
SHergibo <sachau888@hotmail.com>
2022-01-27 16:04:00 +0100

    feat: Added adminTwoDataComplete object in test-data.js.

--- tests/test-data.js
+++ tests/test-data.js
@@ -6,3 +6,12 @@ module.exports.adminOneDataComplete = {
   password: "123456789",
   role: "admin",
 };
+
+module.exports.adminTwoDataComplete = {
+  username: "Pierre Doe",
+  firstname: "Pierre",
+  lastname: "Doe",
+  email: "pierredoe@test.com",
+  password: "123456789",
+  role: "admin",
+};
